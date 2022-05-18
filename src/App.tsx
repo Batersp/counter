a841b0598a59a5a00d8e83ca6772f32857beea13
pavel_ishmukov <p-pup1@mail.ru>
2022-05-19 00:28:55 +0300

    without localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Counter} from "./components/Counter";
+import {Settings} from "./components/Settings";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [limitedValue, setLimitedValue] = useState(5) //макс значение для счетчика
+    const [incDisabled, setIncDisabled] = useState(false) // кнопка inc
+    const [resDisabled, setResDisabled] = useState(true)  // кнопка res
+    const [setDisabled, setSetDisabled] = useState(true) // кнопка set
+    const [maxValue, setMaxValue] = useState(limitedValue)        // максимальное значение в setting
+    const [startValue, setStartValue] = useState(0)      // стартовое значение в setting
+    const [number, setNumber] = useState<number>(startValue)      // число которое увеличивается в counter
+    const [showNumber, setShowNumber] = useState(true) //не могу менять счетчик на текст ( 2 варианта)
+
+    const increaseNumber = () => { // что происходит при нажатии на кнопку inc
+        setNumber(number + 1)
+        setResDisabled(false)
+        if (limitedValue -1 === number) {
+            setIncDisabled(true)
+        }
+
+    }
+    const resetNumber = () => {  // что происходит при нажатии на кнопку res
+        setNumber(startValue)
+        setResDisabled(true)
+        setIncDisabled(false)
+    }
+
+    const setSettings = () => {  // что происходит при нажатии на кнопку set
+        setLimitedValue(maxValue)
+        setSetDisabled(true)
+        setNumber(startValue)
+        setIncDisabled(false)
+        setShowNumber(true)
+    }
+
+
+    return (
+        <div className="App">
+            <Counter
+                number={number}
+                increaseNumber={increaseNumber}
+                resetNumber={resetNumber}
+                incDisabled={incDisabled}
+                resDisabled={resDisabled}
+                limitedValue={limitedValue}
+                maxValue={maxValue}
+                startValue={startValue}
+                showNumber={showNumber}
+                setDisabled={setDisabled}
+
+            />
+
+            <Settings
+                setDisabled={setDisabled}
+                maxValue={maxValue}
+                startValue={startValue}
+                setMaxValue={setMaxValue}
+                setStartValue={setStartValue}
+                setSetDisabled={setSetDisabled}
+                setSettings={setSettings}
+                showNumber={showNumber}
+                setShowNumber={setShowNumber}
+                setIncDisabled={setIncDisabled}
+                setResDisabled={setResDisabled}
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Counter.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Button} from "./Button";
+
+export type CounterPropsType = {
+    number: number
+    incDisabled: boolean
+    resDisabled: boolean
+    limitedValue: number
+    startValue: number
+    maxValue: number
+    showNumber: boolean
+    setDisabled: boolean
+    increaseNumber: () => void
+    resetNumber: () => void
+
+
+}
+
+
+export const Counter = (props: CounterPropsType) => {
+
+    const textForCounter = props.maxValue === props.startValue || props.maxValue < 0 || props.startValue < 0 || props.startValue > props.maxValue ? 'error' : 'enter your settings'
+
+    const numberClass = props.limitedValue === props.number || props.maxValue === props.startValue || props.maxValue < 0 || props.startValue < 0 || props.startValue > props.maxValue ? 'count red' : 'count'
+
+    return (
+        <div className='counter'>
+            <div className={ props.setDisabled? numberClass : 'count' }>{props.showNumber
+                ? props.number
+                : textForCounter}</div>
+            <div className='counterBtn'>
+                <div><Button ClassName='btn' name={'inc'} callBack={props.increaseNumber} disabled={props.incDisabled}/></div>
+                <div><Button ClassName='btn' name={'res'} callBack={props.resetNumber} disabled={props.resDisabled}/></div>
+            </div>
+        </div>
+    )
+}
